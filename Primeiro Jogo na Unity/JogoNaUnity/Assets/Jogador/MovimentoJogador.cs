1bb1657e4d7eb6e233f1c61c6f227bc23dba1b0d
Jose-Alberto-Rodrigues-Neto <allio.professional@gmail.com>
2022-11-25 22:53:31 -0300

    Primeiro commit em Estudos-Unity, primeiro projeto de jogo em Unity

---
+++ Primeiro Jogo na Unity/JogoNaUnity/Assets/Jogador/MovimentoJogador.cs
@@ -0,0 +1,21 @@
+//using System.Collections; (por enquanto não será usado)
+//using System.Collections.Generic; (por enquanto não será usado)
+using UnityEngine;
+
+public class MovimentoJogador : MonoBehaviour
+{
+    public Rigidbody rb; //aqui foi criado uma variavel public, do tipo Rigdbody, onde foi dado o nome de rb e no unity foi indexado o rigdbody do jogador, assim, toda vez que essa variavel for escolhida ela estará referenciando o rigdbody do Jogador
+
+    void Start() //Roda quando seu jogo inicia, tipo um create em Game Maker Studio 2
+    {
+        Debug.Log("Olá, mundo"); //manda uma mensagem de debug para o Console do jogo, há outras funções de debug que podem ser usadas, como o Debug.Warning
+
+    }
+
+    void FixedUpdate() //O Update é atualizado uma vez a cada frame por segundo (fps), ou seja, ele irá rodar durante todo o jogo, tipo um step no Game Maker Studio 2, porém é utilizado o "FixedUpdate()" pois assim a Unity entende melhor o código(?), preciso pesquisar sobre isso depois ("Para que serve o FixedUpdate?" e "Diferenças entre o FixedUpdate e o Update") 
+    {
+        //fazendo ele ir infinitamente para frente
+        rb.AddForce(0, 0, 500 * Time.deltaTime); //adiciona uma força/impulso para o personagem, setando o quanto irá mudar seu (x, y, z)
+        //o Time.deltaTime, é utilizado a fim de manter uma taxa de atualização de frame por segundo boa o suficiente para cada computador, exemplo: se um computador roda o jogo a 10 fps, ele irá ser 0.1, porém, se um jogo roda a 20 fps ele irá ser 0.05 (um tempo menor que 0.1, logo, mais rápido)
+    }
+}
