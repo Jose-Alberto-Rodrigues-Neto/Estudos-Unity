c8810ed833738919b67ccd4d28321f45a8a837fa
Jose-Alberto-Rodrigues-Neto <allio.professional@gmail.com>
2022-12-06 23:41:58 -0300

    Adicionando Tela de finalização de jogo e mudança de fases

--- Primeiro Jogo na Unity/JogoNaUnity/Assets/GameManager.cs
+++ Primeiro Jogo na Unity/JogoNaUnity/Assets/GameManager.cs
@@ -5,13 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    bool fimDeJogo = false;
-   public void EndGame()
+    public GameObject UISucesso;
+
+    public bool GameOver = false;
+
+    public void Start() //reseta o UISucesso quando muda de fase, fazendo com que o texto que aparece ao passar de fase volte a ficar invisível
+    {
+        UISucesso.SetActive(false);
+    }
+    public void EndGame()
     {
         float tempoDelay = 1f; //é interna e não pode ser acessada pelos outros módulos
-        if (fimDeJogo == false)
+        if (GameOver == false)
         {
-            fimDeJogo = true;
+            GameOver = true;
             Debug.Log("Game Over");
             Invoke("Restart", tempoDelay); //chama nova cena e da um delay no restarte do jogador
         }
@@ -21,5 +28,10 @@ public class GameManager : MonoBehaviour
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void sucesso()
+    {
+        UISucesso.SetActive(true);
     }
 }
